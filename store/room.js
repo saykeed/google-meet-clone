dac855152c47a8b1664d3992477e049cd48746e8
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2022-03-10 13:20:39 +0100

    turn server validation test

--- store/room.js
+++ store/room.js
@@ -63,6 +63,21 @@ export const actions = {
             'stun:stun2.l.google.com:19302',
           ],
         },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        }
       ],
       iceCandidatePoolSize: 10,
     }
